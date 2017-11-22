403770cc64261ce715cf4987c4c814cd0859edc3
Mattias Fredsberg <mattias.fredsberg@etteplan.com>
2017-11-22 15:04:58 +0100

    Code updated with classes

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var shapes = []
+const shapes = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
@@ -9,12 +9,12 @@ function setup() {
 }
 
 function windowResized() {
-	resizeCanvas(windowWidth, windowHeight)
+  resizeCanvas(windowWidth, windowHeight)
 }
 
 function draw() {
   background(255)
-  shapes.forEach(function(shape) {
+  shapes.forEach(shape => {
     shape.update()
     shape.draw()
   })
@@ -24,49 +24,51 @@ function randomColor() {
   return color(random(255), random(255), random(255), 100)
 }
 
-function Ball() {
-  this.pos = createVector(width / 2, height / 2)
-  this.vel = p5.Vector.random2D().mult(random(0.5, 3))
-  this.r = random(5, 20)
-  this.c = randomColor()
-  
-  this.update = function() {
+class Shape {
+  constructor() {
+    this.pos = createVector(width / 2, height / 2)
+    this.vel = p5.Vector.random2D().mult(random(0.5, 3))
+    this.r = random(5, 20)
+    this.c = randomColor()
+  }
+
+  update() {
     this.pos.add(this.vel)
-    
+
     // If found offscreen, move towards screen
     if (this.pos.x < this.r) this.vel.x = abs(this.vel.x)
     if (this.pos.x > width - this.r) this.vel.x = -abs(this.vel.x)
     if (this.pos.y < this.r) this.vel.y = abs(this.vel.y)
     if (this.pos.y > height - this.r) this.vel.y = -abs(this.vel.y)
+
   }
-  
-  this.draw = function() {
+}
+
+class Ball extends Shape {
+  constructor() {
+    super()
+  }
+
+  draw() {
     noStroke()
     fill(this.c)
     ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2)
   }
 }
 
-function Block() {
-  this.pos = createVector(width / 2, height / 2)
-  this.vel = p5.Vector.random2D().mult(random(0.5, 3))
-  this.r = random(5, 20)
-  this.a = 0
-  this.aVel = random(-0.1, 0.1)
-  this.c = randomColor()
-  
-  this.update = function() {
-    this.pos.add(this.vel)
-    this.a += this.aVel
-    
-    // If found offscreen, move towards screen
-    if (this.pos.x < this.r) this.vel.x = abs(this.vel.x)
-    if (this.pos.x > width - this.r) this.vel.x = -abs(this.vel.x)
-    if (this.pos.y < this.r) this.vel.y = abs(this.vel.y)
-    if (this.pos.y > height - this.r) this.vel.y = -abs(this.vel.y)
+class Block extends Shape {
+  constructor() {
+    super()
+    this.a = 0
+    this.aVel = random(-0.1, 0.1)
   }
-  
-  this.draw = function() {
+
+  update() {
+    super.update()
+    this.a += this.aVel
+  }
+
+  draw() {
     rectMode(CENTER)
     noStroke()
     fill(this.c)
