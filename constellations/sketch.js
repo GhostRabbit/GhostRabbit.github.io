b8967c083fd8ffa5b3c9de0be29cede4310aacd8
Mattias Fredsberg <mattias.fredsberg@etteplan.com>
2017-11-23 08:36:58 +0100

    Code cleanup

--- constellations/sketch.js
+++ constellations/sketch.js
@@ -1,68 +1,63 @@
-var stars = []
- 
+let stars = []
+let recreate
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
-  recreate = true;
+  recreate = true
 }
- 
+
 function createStarField() {
   stars = []
-  for (var i = 0; i < 50; i++) {
-    stars.push(new Star(10 + random(width - 20), 
-                        10 + random(height - 20)))
+  for (let i = 0; i < 50; i++) {
+    stars.push(new Star(random(10, width - 20), random(10, height - 20)))
   }
-  for (var i = 0; i < stars.length; i++) {
-    var minD = [width + height, width + height, width + height]
-    var nearest = []
-    for (var j = 0; j < stars.length; j++) {
+  for (let i = 0; i < stars.length; i++) {
+    const nearest = []
+    for (let j = 0; j < stars.length; j++) {
       if (i != j) {
         var d = stars[i].pos.dist(stars[j].pos)
-        nearest.push({d: d, star: stars[j]})
+        nearest.push({ d: d, star: stars[j] })
       }
     }
-    nearest.sort(function(a, b) {
-      return a.d - b.d
-    })
+    nearest.sort((a, b) => a.d - b.d)
     stars[i].nearest = nearest.splice(0, 2)
   }
 }
- 
+
 function draw() {
   if (recreate) {
     createStarField()
     recreate = false
   }
   background(0, 0, 10)
-  stars.forEach(function(star) {
-    star.show()
-  })
+  stars.forEach(star => star.show())
 }
- 
+
 function mouseReleased() {
-  recreate = true;
-  return false;
+  recreate = true
+  return false
 }
 
 function windowResized() {
-	resizeCanvas(windowWidth, windowHeight)
-	recreate = true;
+  resizeCanvas(windowWidth, windowHeight)
+  recreate = true
 }
- 
-function Star(x, y) {
-  this.pos = createVector(x, y)
-  this.nearest = [];
 
-  this.show = function() {
+class Star {
+  constructor(x, y) {
+    this.pos = createVector(x, y)
+    this.nearest = []
+  }
+
+  show() {
     strokeWeight(1)
-    stroke(255, 100)
-    this.nearest.forEach(function(other) {
-      line(this.pos.x, this.pos.y, other.star.pos.x, other.star.pos.y)
-     
-    }.bind(this))
- 
+    stroke(100)
+    this.nearest
+      .filter(other => other.d < width / 6)
+      .forEach(other => line(this.pos.x, this.pos.y, other.star.pos.x, other.star.pos.y))
+
     noStroke()
     fill(255)
     ellipse(this.pos.x, this.pos.y, 4, 4)
   }
- 
 }
