b0abc71b17538048ba58ed18a3c091e6058fd34d
Mattias Fredsberg <mattias@fredsberg.se>
2023-06-19 18:20:03 +0200

    Cleanup, upgrade lib

--- rotate3D/sketch.js
+++ rotate3D/sketch.js
@@ -1,21 +1,23 @@
-var angle = 0
+var angle = 0;
 
 function setup() {
-  createCanvas(400, 400, WEBGL)  
+  createCanvas(400, 400, WEBGL);
+  noStroke()
+  fill(255,215,0)
 }
 
-function update()   {
-  angle += 0.01
+function update() {
+  angle += 0.01;
 }
 
 function draw() {
-  update()
+  update();
   
-  background(50)
+  background(50);
   
-  translate(0, 0, map(constrain(mouseX, 0, 400), 100, 300, -100, 50)) // Zoom
-  rotateX(angle)
-  rotateY(angle * 0.6)
-  
-  torus(100)
+  translate(0, 0, map(constrain(mouseX, 0, 400), 100, 300, -100, 50)); // Zoom
+  rotateX(angle);
+  rotateY(angle * 0.6);
+
+  torus(100);
 }
